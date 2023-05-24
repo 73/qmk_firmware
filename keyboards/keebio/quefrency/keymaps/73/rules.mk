04142509dbab6253da65d039bc065acc3ede091d
73 <73@users.noreply.github.com>
2023-05-24 11:09:47 +0200

    73: keebio/quefrency

---
+++ keyboards/keebio/quefrency/keymaps/73/rules.mk
@@ -0,0 +1,8 @@
+# BOOTLOADER = atmel-dfu # comment this line if you use arduinos
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_caps_word.md
+CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_combo.md
+COMBO_ENABLE = yes
