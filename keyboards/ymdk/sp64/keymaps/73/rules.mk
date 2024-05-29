3b5905fe4a201342c58e572e085f0e2dedb1bee0
73 <529346+73@users.noreply.github.com>
2024-05-29 21:43:12 +0200

    73: ymdk/sp64

---
+++ keyboards/ymdk/sp64/keymaps/73/rules.mk
@@ -0,0 +1,7 @@
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_caps_word.md
+CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_combo.md
+COMBO_ENABLE = yes
